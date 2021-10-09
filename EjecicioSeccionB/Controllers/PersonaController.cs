5335344f117fc458b2b554bbb28ccfa018f6c1ed
gmendez2211 <gmendez.2211@gmail.com>
2021-10-09 00:24:03 -0600

    Primera Version
    Primer Version

---
+++ EjecicioSeccionB/Controllers/PersonaController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EjecicioSeccionB.Connection;
+using EjecicioSeccionB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjecicioSeccionB.Controllers
+{
+    public class PersonaController : Controller
+    {
+        private readonly ILogger<PersonaController> _logger;
+        private readonly Conn _context;
+
+        public PersonaController(Conn context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Personas()
+        {
+            return View();
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.tbl_personas.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Personas([Bind("CodigoPersona, NombrePersona, ApellidoPersona, Edad, EstadoPersona")] PersonaModel personamodel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(personamodel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(personamodel);
+        }
+        
+    }
+}
---
+++ EjecicioSeccionB/Repository/PersonaRepository.cs
@@ -0,0 +1,53 @@
+﻿using EjecicioSeccionB.Connection;
+using EjecicioSeccionB.Models;
+using EjecicioSeccionB.Repository.iRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjecicioSeccionB.Repository
+{
+    public class PersonaRepository : iPersonaRepository
+    {
+        private readonly Conn _db;
+
+        public PersonaRepository(Conn db)
+        {
+            _db = db;
+        }
+
+        public bool ActualizarPersona(PersonaModel persona)
+        {
+            _db.tbl_personas.Update(persona);
+            return GuardaPersona();
+        }
+
+        public bool BorrarPersona(PersonaModel persona)
+        {
+            _db.tbl_personas.Remove(persona);
+            return GuardaPersona();
+        }
+
+        public bool CreaPersona(PersonaModel persona)
+        {
+            _db.tbl_personas.Add(persona);
+            return GuardaPersona();
+        }
+
+        public PersonaModel GetPersona(int pCodigoInterno)
+        {
+            return _db.tbl_personas.FirstOrDefault(p=>p.CodigoPersona == pCodigoInterno);
+        }
+
+        public ICollection<PersonaModel> GetPersonaModels()
+        {
+            return _db.tbl_personas.OrderBy(p=>p.ApellidoPersona).ToList();
+        }
+
+        public bool GuardaPersona()
+        {
+            return _db.SaveChanges() >= 0 ? true:false;
+        }
+    }
+}
